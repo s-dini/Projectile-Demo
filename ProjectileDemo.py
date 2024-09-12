571b65f2ad9c8b4d842711f48024212a38de4a2b
Sabrina Dini <sabrina.dini02@gmail.com>
2024-09-12 13:32:33 -0400

    1.1
    Refined to include initial position

--- ProjectileDemo.py
+++ ProjectileDemo.py
@@ -2,37 +2,43 @@ import matplotlib.pyplot as plt
 import numpy as np
 import math
 
-def plot_graph(velocity_initial, angle, ax, title="Graph", xlabel="X-axis", ylabel="Y-axis", line_style="-", color="blue", marker=None, show=False):
-    t = np.linspace(0, 5, endpoint=True)  # Time array
-    X = ReturnX(velocity_initial, angle, t) 
-    Y = ReturnY(velocity_initial, angle, t)
-    
+def plot_graph(velocity_initial, angle, initial_position, ax, title="Graph", xlabel="X-axis", ylabel="Y-axis", line_style="-", color="blue", marker=None, show=False):
+    t = np.linspace(0, 10, endpoint=True)
+    X = ReturnX(velocity_initial, angle, initial_position[0], t) 
+    Y = ReturnY(velocity_initial, angle, initial_position[1], t)
+
     ax.plot(X, Y, linestyle=line_style, color=color, marker=marker, label=f'Angle: {angle}°')
 
 def CalculateComponents(vi, angle): 
-    angle_rad = np.radians(angle)  # Convert angle to radians
+    angle_rad = np.radians(angle)  
     vx = vi * math.cos(angle_rad) 
     vy = vi * math.sin(angle_rad)
     return (vx, vy)
 
-def ReturnX(vi, angle, t): 
-    angle_rad = np.radians(angle)  # Convert angle to radians
+def ReturnX(vi, angle, x, t): 
+    angle_rad = np.radians(angle)  
     vx = vi * math.cos(angle_rad)
-    return vx * t  # Horizontal position as a function of time
+    return (vx * t + x)
 
-def ReturnY(vi, angle, t): 
-    angle_rad = np.radians(angle)  # Convert angle to radians
+def ReturnY(vi, angle, y, t): 
+    angle_rad = np.radians(angle)  
     vy = vi * math.sin(angle_rad)
-    return ( -0.5 * 9.8 * t ** 2.0 ) + ( vy * t )  # Vertical position as a function of time
+    return (( -0.5 * 9.8 * t ** 2.0 ) + ( vy * t ) + y )  # Vertical position as a function of time
 
 # Create figure and axes: fig, ax returns both the figure and the axes 
 fig, ax = plt.subplots(figsize=(10, 10))
 
-# Plot multiple lines
-plot_graph(50, 45, ax, line_style="-", color="blue")
-plot_graph(50, 30, ax, line_style="-", color="green")
+plot_graph(50, 45, (10,0), ax, line_style="-", color="blue")
+plot_graph(50, 30, (0,0), ax, line_style="-", color="green")
+plot_graph(25, 10, (0,30), ax, line_style="-", color="brown")
 
-# Set titles and labels
+plt.autoscale(enable=True, axis="both", tight=None)
+
+ax = plt.gca()
+
+ax.set_xlim(xmin=0)
+ax.set_ylim(ymin=0)
+
 ax.set_title("Projectile Motion (Parametric)")
 ax.set_xlabel("Horizontal Distance (m)")
 ax.set_ylabel("Vertical Distance (m)")
