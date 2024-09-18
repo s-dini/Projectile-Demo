f2e5f495b0cf1bbd09136e719deef93d6013e535
Sabrina Dini <sabrina.dini02@gmail.com>
2024-09-18 16:10:49 -0400

    1.3.1

--- RecordsDemo.py
+++ RecordsDemo.py
@@ -22,34 +22,70 @@ def create_dictionary( en_key ):
 cont = True
 
 while (cont):
-    response1 = input("Would you like to encrypt or decrypt a word? (You must know the encryption key for both!)(Y/N) ")
-    
-    print("")
-    encryption_key = int ( input("Enter an encryption key. ") )
-    encrypted_text = ""
-
-    caesar = create_dictionary(encryption_key)
-
-    print("")
-    text = input("Enter your text to encrypt: ").upper()
-    print(text)
-
-    print("")
-
-    for letter in text:
-        
-        if ( 'A' <= letter <= 'Z' ):
-            num = ord(letter)
-            encrypted_text += caesar[num - 65][chr(num)]
-        
-        else:
-            encrypted_text += letter
-
-    print(encrypted_text)
-
-    response1 = input("Would you like to continue? (Y/N) ").upper()
+    response1 = input("Would you like to encrypt or decrypt a word? (You must know the encryption key for both!)(Y/N) ").upper()
 
     if (response1 == "N"):
+        cont = False
+        break
+
+    elif (response1 == "Y"):
+        response2 = input("Encrypt or Decrypt? (E/D) ").upper()
+
+        if (response2 == "E"):
+
+            print("")
+            encryption_key = int ( input("Enter an encryption key. ") )
+            encrypted_text = ""
+
+            caesar = create_dictionary(encryption_key)
+
+            print("")
+            text = input("Enter your text to encrypt: ").upper()
+            print(text)
+
+            print("")
+
+            for letter in text:
+                
+                if ( 'A' <= letter <= 'Z' ):
+                    num = ord(letter)
+                    encrypted_text += caesar[num - 65][chr(num)]
+                
+                else:
+                    encrypted_text += letter
+
+            print(encrypted_text)
+
+        
+        elif (response2 == "D"):
+
+            print("")
+            encryption_key = int ( input("Enter an encryption key. ") )
+            encrypted_text = ""
+
+            caesar = create_dictionary(encryption_key)
+
+            print("")
+            text = input("Enter your text to decrypt: ").upper()
+            print(text)
+
+            print("")
+
+            for letter in text:
+                
+                if ( 'A' <= letter <= 'Z' ):
+                    num = ord(letter)
+                    encrypted_text += caesar[num - 65][chr(num)]
+                
+                else:
+                    encrypted_text += letter
+
+            print(encrypted_text)
+
+
+    response3= input("Would you like to continue? (Y/N) ").upper()
+
+    if (response3 == "N"):
         cont = False
         print("")
         print("Bye!")
